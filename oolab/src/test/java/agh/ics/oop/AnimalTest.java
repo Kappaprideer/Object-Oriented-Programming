60209e9f99d01d8c549843d96eb1e079396b265f
Endrew <azab@student.agh.edu.pl>
2022-10-19 12:09:57 +0200

    added Tests

--- oolab/src/test/java/agh/ics/oop/AnimalTest.java
+++ oolab/src/test/java/agh/ics/oop/AnimalTest.java
@@ -6,36 +6,75 @@ import org.junit.jupiter.api.Assertions;
 public class AnimalTest {
 
     @Test
-    public void Animal_1(){
+    public void Righ_and_Up_Border_Test(){
         Animal animal = new Animal();
-
-        Assertions.assertEquals(animal.toString(), "pozycja: (,) orientacja: ");
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(), "pozycja: (2,4) orientacja: Wschód");
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(), "pozycja: (4,4) orientacja: Południe");
     }
 
     @Test
-    public void Animal_2(){
+    public void Lef_and_Bottom_Border_Test(){
         Animal animal = new Animal();
-
-        Assertions.assertEquals(animal.toString(), "pozycja: (,) orientacja: ");
+        animal.move(MoveDirection.LEFT);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        Assertions.assertEquals(animal.toString(), "pozycja: (0,2) orientacja: Zachód");
+        animal.move(MoveDirection.LEFT);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.FORWARD);
+        animal.move(MoveDirection.RIGHT);
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(), "pozycja: (0,0) orientacja: Północ");
     }
 
     @Test
-    public void Animal_3(){
+    public void OrientationTest_1(){
         Animal animal = new Animal();
-
-        Assertions.assertEquals(animal.toString(), "pozycja: (,) orientacja: ");
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Wschód");
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Południe");
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Zachód");
+        animal.move(MoveDirection.RIGHT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Północ");
     }
 
     @Test
-    public void Animal_4(){
+    public void OrientationTest_2(){
         Animal animal = new Animal();
-
-        Assertions.assertEquals(animal.toString(), "pozycja: (,) orientacja: ");
+        animal.move(MoveDirection.LEFT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Zachód");
+        animal.move(MoveDirection.LEFT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Południe");
+        animal.move(MoveDirection.LEFT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Wschód");
+        animal.move(MoveDirection.LEFT);
+        Assertions.assertEquals(animal.toString(),"pozycja: (2,2) orientacja: Północ");
     }
+
     public void Animal_5(){
         Animal animal = new Animal();
-
-        Assertions.assertEquals(animal.toString(), "pozycja: (,) orientacja: ");
+        String[] input = {"f", "f", "forward", "right", "f", "f", "f", "left", "l"};
+        OptionsParser parser = new OptionsParser();
+        MoveDirection[] moves = parser.parse(input);
+        for(MoveDirection move : moves)
+            animal.move(move);
+        Assertions.assertEquals(animal.toString(), "pozycja: (4,4) orientacja: Zachód");
     }
 
 
--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -18,4 +18,28 @@ public class OptionsParserTest {
         Assertions.assertEquals(animal.toString(),"pozycja: (3,2) orientacja: Wschód");
     }
 
+    @Test
+    public void ParseTest_2(){
+        Animal animal = new Animal();
+        String[] arguments = {"f","f","r","f","f","r","f","f","g","foo","r"};
+        OptionsParser parser= new OptionsParser();
+        MoveDirection[] moves = parser.parse(arguments);
+        for(MoveDirection move : moves){
+            animal.move(move);
+        }
+        Assertions.assertEquals(animal.toString(),"pozycja: (4,2) orientacja: Zachód");
+    }
+
+    @Test
+    public void ParseTest_3() {
+        Animal animal = new Animal();
+        String[] arguments = {"foo", "feee", "fr", "bl", "bb", "ll", "lb"};
+        OptionsParser parser = new OptionsParser();
+        MoveDirection[] moves = parser.parse(arguments);
+        for (MoveDirection move : moves)
+            animal.move(move);
+        Assertions.assertEquals(animal.toString(), "pozycja: (2,2) orientacja: Północ");
+    }
+
+
 }
