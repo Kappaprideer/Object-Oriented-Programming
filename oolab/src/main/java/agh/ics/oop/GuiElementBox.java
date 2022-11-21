c0608e35ea42afc9a7ab418a0cd18c12f2e441d6
Endrew <azab@student.agh.edu.pl>
2022-11-21 17:37:44 +0100

    Lab8

---
+++ oolab/src/main/java/agh/ics/oop/GuiElementBox.java
@@ -0,0 +1,25 @@
+package agh.ics.oop;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+
+
+
+public class GuiElementBox {
+    Image image;
+    ImageView imageView;
+    Label label;
+    public GuiElementBox(IMapElement mapElement) throws FileNotFoundException{
+        this.image= new Image(new FIle);
+        imageView.setFitWidth(20);
+        imageView.setFitHeight(20);
+
+    }
+
+
+    VirtualBox vbox = new GuiElementBox();
+
+}
